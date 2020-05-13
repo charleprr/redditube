02905793e319c479380283e78d542d8f7f30b4b6
Charly Poirier <pro.poiriercharly@gmail.com>
2020-05-13 23:25:24 +0100

    index.js structure

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * @name Redditube
+ * @version 1.0.0
+ * 
+ * From posts and comments on Reddit
+ * to a video uploaded on Youtube!
+ * 
+ * @copyright (C) 2020 by Charly Poirier
+*/
+
+/* Import modules */
+
+(async () => {
+
+    console.log("Hello, world!");
+
+    /* Step 1 */
+
+    /* Step 2 */
+    
+    /* Step 3 */
+    
+    /* Step 4 */
+
+})();
