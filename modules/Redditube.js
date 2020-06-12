309f431f9428b8c2308b8574a68131d07fa6f99e
Charly Poirier <pro.poiriercharly@gmail.com>
2020-06-12 12:57:23 +0100

    Temporary files are now automatically deleted

--- modules/Redditube.js
+++ modules/Redditube.js
@@ -7,6 +7,8 @@
  * 
  * @copyright (C) 2020 by Charly Poirier
 */
+const fs = require(`fs`);
+const path = require(`path`);
 const { performance } = require(`perf_hooks`);
 const Reddit = require(`./Reddit.js`);
 const Image = require(`./Image.js`);
@@ -24,22 +26,27 @@ module.exports = {
         const post = await Reddit.fetchPost(subreddit, sort, time);
         const comments = await Reddit.fetchComments(subreddit, post.id, count);
 
+        await Image.generate(post, comments, subreddit);
+        await Sound.generate(post, comments);
+        await Video.generate(post, comments);
+
+        fs.readdir(`tmp`, (err, files) => {
+            if (err) throw err;
+            for (const file of files) {
+                fs.unlink(`tmp/${file}`, err => {
+                    if (err) throw err;
+                });
+            }
+        });
+        
         const title = `${post.title} - ${subreddit}`;
         const description = `${title}\n#reddit #stories`;
         const tags = post.title.split(` `);
         const privacy = `unlisted`;
 
-        await Image.generate(post, comments, subreddit);
-        await Sound.generate(post, comments);
-        await Video.generate(post, comments);
-
-        /* Clean temporary folder (synchronous) */
-
         await YouTube.upload(title, description, tags, privacy);
 
-        const seconds = (performance.now() - t0) / 1000;
-        console.log(`Execution time: ${Math.round(seconds/60)} minutes ${Math.round(seconds%60)} seconds`);
-
+        console.log(`Done! Took ${Math.round((performance.now() - t0) / 1000 / 60)} minutes`);
         resolve();
 
     })
