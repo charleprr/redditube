a30469f514b35c1e0ad74a6e6aaa4988e062ed1e
Charly Poirier <pro.poiriercharly@gmail.com>
2020-06-19 20:53:36 +0100

    Completely new way of getting Reddit submissions and comments

--- modules/Reddit.js
+++ modules/Reddit.js
@@ -8,109 +8,50 @@
  * @copyright (C) 2020 by Charly Poirier
 */
 
-// 60 reqs per minute
-
-const rp = require('request-promise');
+const Snoowrap = require(`snoowrap`);
+const configuration = require(`../config.json`);
+const reddit = new Snoowrap(configuration);
 
 module.exports = {
 
-    /**
-     * Fetch a random post from Reddit.
-     * 
-     * @param {String} subreddit  The subreddit to fetch a post from.
-     * @param {String} [sort=top] Sorting (relevance, hot, top, new, comments).
-     * @param {String} [time=all] Time (hour, day, week, month, year, all).
-     * @param {Number} [limit=30] 
-     * 
-     * @return {Object} Reddit post.
-     */
-    fetchRandomPost: (subreddit, sort=`top`, time=`all`, limit=25) => new Promise(async resolve => {
-
-        const res = await rp({
-            uri: `https://www.reddit.com/${subreddit}/${sort}.json?&t=${time}&limit=${limit}`,
-            json: true
-        });
-
-        let post = res.data.children[Math.floor(Math.random()*limit)].data;
-        post = {
-            id: post.id,
-            title: post.title,
-            body: post.body,
-            author: post.author,
-            ups: post.ups,
-            awards: post.all_awardings.map(award => Object({
-                name: award.name, count: award.count, url: award.icon_url
+    fetch: (id) => new Promise (async resolve => {
+        const submission = await reddit.getSubmission(id).fetch();
+        resolve({
+            id: submission.id,
+            subreddit: submission.subreddit.display_name,
+            title: submission.title,
+            body: submission.body,
+            author: submission.author.name,
+            ups: submission.ups,
+            awards: submission.all_awardings.map(award => ({
+                url: award.icon_url,
+                count: award.count
+            })),
+            comments: submission.comments.map(comment => ({
+                id: comment.id,
+                author: comment.author.name,
+                body: comment.body,
+                ups: comment.ups,
+                awards: comment.all_awardings.map(award => ({
+                    url: award.icon_url,
+                    count: award.count
+                })),
+                replies: comment.replies.map(reply => ({
+                    id: reply.id,
+                    body: reply.body,
+                    author: reply.author.name,
+                    ups: reply.ups,
+                    awards: reply.all_awardings.map(award => ({
+                        url: award.icon_url,
+                        count: award.count
+                    }))
+                }))
             }))
-        };
-
-        console.log(`Fetched post « ${post.title} »`);
-        return resolve(post);
-
+        });
     }),
 
-    fetchComments: (subreddit, post_id, count, sort=`top`, time=`all`) => new Promise(async resolve => {
-
-        console.log(`Fetching comments`);
-        
-        const res = await rp({
-            uri: `https://www.reddit.com/${subreddit}/comments/${post_id}/${sort}.json?t=${time}`,
-            json: true
-        });
-
-        const comments = [];
-        for (let comment of res[1].data.children.slice(0, count)) {
-            comment = {
-                id: comment.data.id,
-                link_id: comment.data.link_id.substr(3),
-                author: comment.data.author,
-                body: comment.data.body,
-                paragraphs: comment.data.body.split(/\n(?![^\n])/g),
-                ups: comment.data.ups,
-                awards: comment.data.all_awardings.map(award => Object({
-                    name: award.name, count: award.count, url: award.icon_url
-                }))
-            };
-
-            /*
-            console.log(comment.body);
-            console.log(comment.paragraphs);
-            console.log(`-------------------------------------------------`);
-            */
-
-            if (comment.body.length < 1000) {
-
-                const res = await rp({
-                    uri: `https://www.reddit.com/r/askreddit/comments/${comment.link_id}/_/${comment.id}.json`,
-                    json: true
-                });
-
-                const replies = res[1].data.children[0].data.replies.data.children;
-                
-                let maxUps = 0;
-                for (const reply of replies) {
-                    if (reply.data.ups > maxUps) {
-                        maxUps = reply.data.ups;
-                        comment.reply = {
-                            id: reply.data.id,
-                            body: reply.data.body,
-                            paragraphs: reply.data.body.split(/\n(?![^\n])/g),
-                            author: reply.data.author,
-                            ups: reply.data.ups,
-                            awards: reply.data.all_awardings.map(award => Object({
-                                name: award.name,
-                                count: award.count,
-                                url: award.icon_url
-                            }))
-                        };
-                    }
-                }
-            }
-
-            comments.push(comment);
-        }
-        
-        console.log(`Fetched ${comments.length} comments`);
-        resolve(comments);
+    fetchRandom: (subreddit, count, sort, time) => new Promise (resolve => {
+        resolve(`Not supported yet.`);
     })
-    
+
 }
